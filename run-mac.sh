07dc509fbd01a22e204c85bbf3b44cc6a0197476
李梦龙 <649076408@qq.com>
2020-10-07 14:04:30 +0800

    feat($yarn): config yarn version by yarn2

--- run-mac.sh
+++ run-mac.sh
@@ -1,3 +1,5 @@
+if [ -d ./www/.yarn ]; then rm -rf ${result_path}www/.yarn; fi
+if [ -f ./www/.yarnrc ]; then rm ${result_path}www/.yarnrc; fi
 if [ -f .env ]; then rm ${result_path}.env; fi
 if [ -f docker-compose.yml ]; then rm ${result_path}docker-compose.yml; fi
 
