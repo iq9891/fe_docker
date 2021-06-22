7a7209053a8ebfac81b4c3d76c0768ba5ab2d372
李梦龙 <649076408@qq.com>
2021-06-23 01:42:00 +0800

    fix($mac): build error

--- run-mac.sh
+++ run-mac.sh
@@ -3,7 +3,17 @@ if [ -f ./www/.yarnrc ]; then rm ${result_path}www/.yarnrc; fi
 if [ -f .env ]; then rm ${result_path}.env; fi
 if [ -f docker-compose.yml ]; then rm ${result_path}docker-compose.yml; fi
 
-cp .env.sample .env
-cp docker-compose.sample.yml docker-compose.yml
+
+# 如果存在就复制
+if [ ! -f ".env.sample .env" ];then
+  sysOS=`uname -s`
+  if [ $sysOS == "Darwin" ];then
+    cp .env.sample .env
+    cp docker-compose.sample.yml docker-compose.yml
+  else
+    copy .env.sample .env
+    copy docker-compose.sample.yml docker-compose.yml
+  fi
+fi
 
 docker-compose up -d
